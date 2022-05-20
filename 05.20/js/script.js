452b325b70e8f5a32b01d2fd00c4be1d0664fa9f
Qiyasjafarov <100667434+Qiyasjafarov@users.noreply.github.com>
2022-05-20 08:36:26 +0400

    Update script.js

--- 05.20/js/script.js
+++ 05.20/js/script.js
@@ -10,23 +10,7 @@ let imageContainer = document.querySelector("#image-container");
 
 
 submitBtn.addEventListener("click",(e)=>{
-    fileUpload.addEventListener("change", function (e) {
-        let uploadedFiles = e.target.files;
     
-        for (const file of uploadedFiles) {
-            let reader = new FileReader();
-            reader.onloadend = (e) => {
-                let elem = `
-                <div class="image-wrapper">
-                    <span class="image-remover">×</span>
-                    <img src="${e.target.result}" alt="">
-                </div>
-                `
-                infoContainer.innerHTML += elem;
-            }
-            reader.readAsDataURL(file);
-        }
-    })
     login.classList.add("d-none");
     infoContainer.classList.remove("d-none");
     for(let i = 0; i<inputs.length;i++){
@@ -35,9 +19,24 @@ submitBtn.addEventListener("click",(e)=>{
             infoContainer.innerHTML +=inputInfo;
         }
     }
-    
-
-
 })
+
+fileUpload.addEventListener("change", function (e) {
+        let uploadedFiles = e.target.files;
+    
+        for (const file of uploadedFiles) {
+            let reader = new FileReader();
+            reader.onloadend = (e) => {
+                let elem = `
+                <div class="image-wrapper">
+                   
+                    <img src="${e.target.result}" alt="">
+                </div>
+                `
+                infoContainer.innerHTML += elem;
+            }
+            reader.readAsDataURL(file);
+        }
+    })
 
 
