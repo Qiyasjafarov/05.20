c05a7ce540e9da9db29f65a72811a12684d52ad3
Qiyasjafarov <100667434+Qiyasjafarov@users.noreply.github.com>
2022-05-20 05:07:30 +0400

    Add files via upload

---
+++ 05.20/js/script.js
@@ -0,0 +1,43 @@
+"use strict"
+
+let inputs = document.querySelectorAll(".login-box input");
+let login = document.querySelector(".login-box");
+let infoContainer = document.querySelector(".info");
+let submitBtn=document.querySelector(".submit-button");
+let fileUpload = document.querySelector(".file-input");
+let uploadBtn = document.querySelector(".fake-btn");
+let imageContainer = document.querySelector("#image-container");
+
+
+submitBtn.addEventListener("click",(e)=>{
+    fileUpload.addEventListener("change", function (e) {
+        let uploadedFiles = e.target.files;
+    
+        for (const file of uploadedFiles) {
+            let reader = new FileReader();
+            reader.onloadend = (e) => {
+                let elem = `
+                <div class="image-wrapper">
+                    <span class="image-remover">×</span>
+                    <img src="${e.target.result}" alt="">
+                </div>
+                `
+                infoContainer.innerHTML += elem;
+            }
+            reader.readAsDataURL(file);
+        }
+    })
+    login.classList.add("d-none");
+    infoContainer.classList.remove("d-none");
+    for(let i = 0; i<inputs.length;i++){
+        if(!inputs[i].value.trim()==""){
+            let inputInfo = `<p>${inputs[i].value}</p>`;
+            infoContainer.innerHTML +=inputInfo;
+        }
+    }
+    
+
+
+})
+
+
